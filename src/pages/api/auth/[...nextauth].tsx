0db6d834bb2af6561fd3669e1e05150e1f202eee
Aleem Beyg <111571595+aleembeyg@users.noreply.github.com>
2023-01-30 13:36:28 +0500

    Server Configuration
    Server Configuration

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,11 +1,17 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
 export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.NEXTAUTH_GOOGLE_APP_ID || "",
       clientSecret: process.env.NEXTAUTH_GOOGLE_SECRET_ID || "",
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
     }),
   ],
 });
