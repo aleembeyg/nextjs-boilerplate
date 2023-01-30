eacb158cecd9b72c219e2c7579d50b67f0e8885a
Aleem Beyg <111571595+aleembeyg@users.noreply.github.com>
2023-01-30 14:37:20 +0500

    UI Fixes
    UI Fixes

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -4,6 +4,7 @@ import { FormattedMessage } from "react-intl";
 import styles from "@/styles/Layout.module.css";
 import { useRouter } from "next/router";
 import { useSession, signOut } from "next-auth/react";
+import { BsCreditCard2FrontFill } from "react-icons/bs";
 
 type IMainProps = {
   children: ReactNode;
@@ -20,81 +21,129 @@ const Layout = (props: IMainProps) => {
   const { data: session } = useSession();
   return (
     <div className={`container-fluid ${styles.header_FC}`}>
-      <header style={{ backgroundColor: "#eee" }}>
-        <div className={styles.header}>
-          <h1 className="p-3" style={{ fontSize: "25px", fontWeight: "bold" }}>
-            <Link href={"/"}>
-              <FormattedMessage id="page.home.head.title" />
-            </Link>
-          </h1>
-          <nav
-            style={{
-              minWidth: "310px",
-              display: "flex",
-              justifyContent: "space-between",
-            }}
-          >
-            <span>
-              <button
-                role="button"
-                style={{ cursor: "pointer", border: 0 }}
-                onClick={() => {
-                  handleChangeLanguage("en");
-                }}
+      <div className="bg-light">
+        <nav
+          style={{ maxWidth: "1100px", margin: "auto" }}
+          className="navbar navbar-expand-lg navbar-light bg-light"
+        >
+          <div className="container-fluid">
+            <h1
+              className="navbar-brand p-3"
+              style={{ fontSize: "25px", fontWeight: "bold" }}
+            >
+              <Link
+                href={"/"}
+                style={{ display: "flex", alignItems: "center" }}
               >
-                EN
-              </button>{" "}
-              |{" "}
-              <button
-                role="button"
-                style={{ cursor: "pointer", border: 0 }}
-                onClick={() => {
-                  handleChangeLanguage("fr");
-                }}
-              >
-                FR
-              </button>{" "}
-              |{" "}
-              <button
-                role="button"
-                style={{ cursor: "pointer", border: 0 }}
-                onClick={() => {
-                  handleChangeLanguage("ar");
-                }}
-              >
-                AR
-              </button>
-            </span>
-            &nbsp;
-            <Link href={"/contact-us"}>
-              <FormattedMessage id="page.home.link.contactus" />
-            </Link>
-            &nbsp;&nbsp;
-            <Link href={"/users"}>
-              <FormattedMessage id="page.home.link.users" />
-            </Link>
-            &nbsp;&nbsp;
-            {session == null && (
-              <Link href={"/login"}>
-                <FormattedMessage id="page.home.link.login" />
+                <BsCreditCard2FrontFill style={{ width: "25px" }} />
+                &nbsp;
+                <FormattedMessage id="page.home.head.title" />
               </Link>
-            )}
-            {session && (
-              <button
-                role="button"
-                style={{ cursor: "pointer", border: 0 }}
-                onClick={() => {
-                  handleLogout();
-                }}
-              >
-                Logout
-              </button>
-            )}
-          </nav>
-        </div>
-      </header>
+            </h1>
+            <button
+              type="button"
+              className="navbar-toggler"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarCollapse"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+              <div className="navbar-nav"></div>
+              <div className="navbar-nav ms-auto">
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-evenly",
+                    alignItems: "center",
+                  }}
+                >
+                  <button
+                    role="button"
+                    style={{
+                      cursor: "pointer",
+                      border: 0,
+                      background: "none",
+                      color: "#333",
+                    }}
+                    className="nav-item nav-link"
+                    onClick={() => {
+                      handleChangeLanguage("en");
+                    }}
+                  >
+                    EN
+                  </button>{" "}
+                  |{" "}
+                  <button
+                    role="button"
+                    style={{
+                      cursor: "pointer",
+                      border: 0,
+                      background: "none",
+                      color: "#333",
+                    }}
+                    className="nav-item nav-link"
+                    onClick={() => {
+                      handleChangeLanguage("fr");
+                    }}
+                  >
+                    FR
+                  </button>{" "}
+                  |{" "}
+                  <button
+                    role="button"
+                    style={{
+                      cursor: "pointer",
+                      border: 0,
+                      background: "none",
+                      color: "#333",
+                    }}
+                    className="nav-item nav-link"
+                    onClick={() => {
+                      handleChangeLanguage("ar");
+                    }}
+                  >
+                    AR
+                  </button>
+                </div>
+                &nbsp;
+                <Link href={"/contact-us"} className="nav-item nav-link">
+                  <FormattedMessage id="page.home.link.contactus" />
+                </Link>
+                &nbsp;&nbsp;
+                <Link href={"/users"} className="nav-item nav-link">
+                  <FormattedMessage id="page.home.link.users" />
+                </Link>
+                &nbsp;&nbsp;
+                {session == null && (
+                  <Link href={"/login"} className="nav-item nav-link">
+                    <FormattedMessage id="page.home.link.login" />
+                  </Link>
+                )}
+                {session && (
+                  <button
+                    role="button"
+                    style={{
+                      cursor: "pointer",
+                      border: 0,
+                      background: "#fff",
+                      color: "#333",
+                    }}
+                    className="nav-item nav-link"
+                    onClick={() => {
+                      handleLogout();
+                    }}
+                  >
+                    Logout
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
       <div>{props.children}</div>
-      <footer className="p-3" style={{ backgroundColor: "#eee" }}>
+      <footer className="bg-light p-3" style={{ backgroundColor: "#eee" }}>
         <div style={{ maxWidth: "1100px", margin: "auto" }}>
           2023 SendCredit
         </div>
