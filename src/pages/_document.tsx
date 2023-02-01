c0728099a5d6958253d9743f84c27ccf71af27e0
Aleem Beyg <111571595+aleembeyg@users.noreply.github.com>
2023-02-01 11:10:40 +0500

    Commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="shortcut icon" href="/icon-192x192.png" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/icon-256x256.png" />
+        <meta name="theme-color" content="#6A359C"/>
         <link rel="manifest" href="/manifest.json" />
       </Head>
       <body>
