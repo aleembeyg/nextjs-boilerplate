03c24f5fdd02d20a02ec98edd8485d93f545ac6b
Aleem Beyg <111571595+aleembeyg@users.noreply.github.com>
2023-02-01 12:01:31 +0500

    Create gatsby-config.js

---
+++ src/gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby`,
+  },
+  plugins: [
+    `gatsby-transform-plugin`,
+    {
+      resolve: `gatsby-plugin-name`,
+      options: {
+        optionA: true,
+        optionB: `Another option`,
+      },
+    },
+  ],
+}
