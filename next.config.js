1c50d01640b0f0438ce73bdf36bd4959efacc55c
Aleem Beyg <111571595+aleembeyg@users.noreply.github.com>
2023-03-09 14:31:45 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,25 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
-  skipWaiting: true
-})
+  skipWaiting: true,
+});
 const nextConfig = withPWA({
   devIndicators: {
     buildActivity: false,
   },
   reactStrictMode: true,
-  i18n: {
-    // These are all the locales you want to support in
-    // your application
-    locales: ["en", "fr", "ar"],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: "en",
-    localeDetection: false,
-  },
+  // i18n: {
+  //   // These are all the locales you want to support in
+  //   // your application
+  //   locales: ["en", "fr", "ar"],
+  //   // This is the default locale you want to be used when visiting
+  //   // a non-locale prefixed path e.g. `/hello`
+  //   defaultLocale: "en",
+  //   localeDetection: false,
+  // },
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    unoptimized: true,
   },
 });
 
